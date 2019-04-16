9022a84e9959a03bfba92ac5d73c1d3a7c0a2401
Tuomas Pöyry <tuomas@munkkeli.io>
2019-04-16 10:58:35 +0300

    Initial Commit

---
+++ Headlight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Headlight
+//
+//  Created by Tuomas Pöyry on 16/04/2019.
+//  Copyright © 2019 iSchoolMusical. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
