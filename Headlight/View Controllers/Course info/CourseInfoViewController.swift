5c43b038770fc0796868e7d3052c6499959915aa
hamedshahidi <hamedshahidiz@gmail.com>
2019-04-30 18:56:41 +0300

    minor changes

--- Headlight/View Controllers/Course info/CourseInfoViewController.swift
+++ Headlight/View Controllers/Course info/CourseInfoViewController.swift
@@ -17,6 +17,7 @@ class CourseInfoViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.tableFooterView = UIView()
+        tableView.separatorColor = UIColor.clear
         
         // for development only
         course = CoreDataHelper.findCourseByID("metropolia-mobile-05")
