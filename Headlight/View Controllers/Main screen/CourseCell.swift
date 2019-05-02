c141c3c1805aa954c180c431dc93e2cd22d31f10
Janne Suotsalo <janne.suotsalo@gmail.com>
2019-05-02 10:20:34 +0300

    added tableview fetching info and profile card

---
+++ Headlight/View Controllers/Main screen/CourseCell.swift
@@ -0,0 +1,13 @@
+//
+//  CourseCell.swift
+//  Headlight
+//
+//  Created by iosdev on 30/04/2019.
+//  Copyright © 2019 iSchoolMusical. All rights reserved.
+//
+
+import UIKit
+
+class CourseCell: UICollectionViewCell {
+    
+}
---
+++ Headlight/View Controllers/Main screen/skillsgained.swift
@@ -0,0 +1,21 @@
+//
+//  skillsgained.swift
+//  Headlight
+//
+//  Created by iosdev on 01/05/2019.
+//  Copyright © 2019 iSchoolMusical. All rights reserved.
+//
+
+import UIKit
+
+class skillsgained: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
